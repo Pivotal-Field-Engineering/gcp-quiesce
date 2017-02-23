a6e09e66840f60f56424bdffb1814d9acf203d75
azwickey-pivotal <vania12R>
2017-02-23 09:31:21 -0500

    initial commit

---
+++ ci/tasks/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo 'Starting BOSH Deployment $DEPLOYMENT_NAME'
+
+#Needed to login
+#$ export BOSH_CLIENT=ci
+#$ export BOSH_CLIENT_SECRET=ci-password
+
+bosh --ca-cert cacert/bosh.pem target $BOSH_URL
+bosh deployment $DEPLOYMENT_FILE
+bosh vms $DEPLOYMENT_NAME
+bosh -n start --force
+bosh vms $DEPLOYMENT_NAME
