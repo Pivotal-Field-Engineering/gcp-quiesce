89bd3af2ca664bb4049d400eba935cecd4c094ff
azwickey-pivotal <vania12R>
2017-02-23 09:45:00 -0500

    initial commit

---
+++ ci/tasks/failing-vms.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo 'Checking BOSH VMs'
+
+#Needed to login
+#$ export BOSH_CLIENT=ci
+#$ export BOSH_CLIENT_SECRET=ci-password
+
+bosh --ca-cert cacert/bosh.pem target $BOSH_URL
+bosh vms >> output.txt
+cat output.txt | grep failing | wc -l > result.txt
+grep 0 result.txt
