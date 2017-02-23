b9b88ecf2090b660cf7243dd82e5c039736f1451
azwickey-pivotal <vania12R>
2017-02-23 14:38:42 -0500

    Adding stop pipeline

---
+++ ci/tasks/stop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Starting BOSH Deployment $DEPLOYMENT_NAME"
+
+#Needed to login
+#$ export BOSH_CLIENT=ci
+#$ export BOSH_CLIENT_SECRET=ci-password
+
+bosh --ca-cert cacert/bosh.pem target $BOSH_URL
+bosh deployment $DEPLOYMENT_FILE
+bosh vms $DEPLOYMENT_NAME
+bosh -n stop --hard --force
+bosh vms $DEPLOYMENT_NAME >> output.txt
+cat output.txt
+#cat output.txt | grep failing | wc -l > result.txt
+#grep 0 result.txt
