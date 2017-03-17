d4a56ecc8e7f62c090a21bfb824f1ab5d2ac410c
azwickey-pivotal <vania12R>
2017-03-17 09:35:52 -0400

    updated to extract deployments

--- ci/tasks/start.sh
+++ ci/tasks/start.sh
@@ -6,10 +6,16 @@ echo "Starting BOSH Deployment $DEPLOYMENT_NAME"
 #$ export BOSH_CLIENT_SECRET=ci-password
 
 bosh --ca-cert cacert/bosh.pem target $BOSH_URL
-bosh deployment $DEPLOYMENT_FILE
-bosh vms $DEPLOYMENT_NAME
-bosh -n start --force
-bosh vms $DEPLOYMENT_NAME >> output.txt
-cat output.txt
-cat output.txt | grep failing | wc -l > result.txt
-grep 0 result.txt
+
+#Loops through specified deployments
+for deployment in ${DEPLOYMENTS//,/ }
+do
+    # call your procedure/other scripts here below
+    echo "Deployment $deployment"
+    bosh vms $deployment
+    bosh download manifest $deployment $deployment.yml
+    bosh deployment $deployment.yml
+    #bosh -n start --force
+    bosh vms $DEPLOYMENT_NAME >> $deployment.txt
+    cat $deployment.txt
+done
