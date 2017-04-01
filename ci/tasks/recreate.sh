67e81bba10313ca5d875e1403aff8de7572eb9db
azwickey-pivotal <vania12R>
2017-04-01 14:03:03 -0500

    added recreate tasks

---
+++ ci/tasks/recreate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Recreating BOSH Deployment $DEPLOYMENT_NAME"
+
+#Needed to login
+#$ export BOSH_CLIENT=ci
+#$ export BOSH_CLIENT_SECRET=ci-password
+
+bosh --ca-cert cacert/bosh.pem target $BOSH_URL
+
+#Loops through specified deployments
+for deployment in ${DEPLOYMENTS//,/ }
+do
+    # call your procedure/other scripts here below
+    echo "Deployment $deployment"
+    bosh vms $deployment
+    bosh download manifest $deployment $deployment.yml
+    bosh deployment $deployment.yml
+    bosh -n recreate --force
+    bosh vms $deployment >> $deployment.txt
+    cat $deployment.txt
+done
