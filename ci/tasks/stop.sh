df77566590c0ed72474988604a69f5e8f53153a2
azwickey-pivotal <vania12R>
2017-04-05 17:51:57 -0500

    update to handle ODB instances

--- ci/tasks/stop.sh
+++ ci/tasks/stop.sh
@@ -8,18 +8,15 @@ bosh --ca-cert cacert/bosh.pem target $BOSH_URL
 
 #Loops through ODB instances
 bosh deployments | grep service- | cut -d \| -f 2
-#deployment=(${DEPLOYMENTS//,/ })
-#for ((i = ${#deployment[@]} - 1;i >= 0;i--))
-#do
-#    curr_deploy=${deployment[i]}
-#    echo "Stopping Deployment $i: $curr_deploy"
-#    bosh vms $curr_deploy
-#    bosh download manifest $curr_deploy $curr_deploy.yml
-#    bosh deployment $curr_deploy.yml
-#    bosh -n stop --hard --force
-#    bosh vms $curr_deploy >> $curr_deploy.txt
-#    cat $curr_deploy.txt
-#done
+for deployment in `bosh deployments | grep service- | cut -d \| -f 2` ; do
+    echo "Stopping Deployment: $deployment"
+    bosh vms $deployment
+    bosh download manifest $deployment $deployment.yml
+    bosh deployment $deployment.yml
+    bosh -n stop --hard --force
+    bosh vms $deployment >> $deployment.txt
+    cat $deployment.txt
+done
 
 #Loops through specified deployments and stop in reverse order
 deployment=(${DEPLOYMENTS//,/ })
